b440998b1d37ad999dbe185e187ba0e40bfc3b80
najipeng <najipeng@meituan.com>
2023-03-09 18:57:54 +0800

    实现开关功能

--- src/main/kotlin/Whisper.kt
+++ src/main/kotlin/Whisper.kt
@@ -1,5 +1,10 @@
 package com.fixuxt.mirai.whisper
 
+import net.mamoe.mirai.console.command.CommandManager
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.command.GroupAwareCommandSender
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.events.GroupTempMessageEvent
@@ -19,18 +24,43 @@ object Whisper : KotlinPlugin(
     }
 ) {
     override fun onEnable() {
-        logger.info { "Whisper Plugin loaded" }
+        WhisperConfig.reload()
+
+        CommandManager.registerCommand(WhisperCommend)
 
         val globalEventChannel = globalEventChannel()
         globalEventChannel.subscribeAlways<GroupTempMessageEvent> {
-            sender.group.members
-                .filter { member -> member.id != sender.id && member.id != bot.id }
-                .forEach { member ->
-                    member.sendMessage(buildMessageChain {
-                        +PlainText("[${sender.group.name}]${sender.nick}:\n")
-                        +it.message
-                    })
-                }
+            if (WhisperConfig.enabledGroups.contains(group.id)) {
+                sender.group.members
+                    .filter { member -> member.id != sender.id && member.id != bot.id }
+                    .forEach { member ->
+                        member.sendMessage(buildMessageChain {
+                            +PlainText("[${sender.group.name}]${sender.nick}:\n")
+                            +it.message
+                        })
+                    }
+            }
         }
+
+        logger.info { "Whisper Plugin loaded" }
     }
+}
+
+object WhisperConfig : AutoSavePluginConfig("WhisperConfig") {
+    val enabledGroups: MutableSet<Long> by value()
+}
+
+object WhisperCommend : CompositeCommand(Whisper, "whisper") {
+    @SubCommand()
+    suspend fun GroupAwareCommandSender.on() {
+        WhisperConfig.enabledGroups.add(group.id)
+        group.sendMessage("whisper已启动，私聊【${bot.nick}】即可全员转发")
+    }
+
+    @SubCommand()
+    suspend fun GroupAwareCommandSender.off() {
+        WhisperConfig.enabledGroups.remove(group.id)
+        group.sendMessage("whisper已关闭")
+    }
+
 }
